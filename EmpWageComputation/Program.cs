d651559f1e92afbf26e4dc040ffd7388bd39c013
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2021-12-01 11:01:19 +0530

    Added Employee Attendence Functionality

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Employee wage computation");
+Random rnd = new Random();
+int num = rnd.Next();
+if (num % 2 == 0)
+{
+    Console.WriteLine("Employ is Present");
+}
+else
+{
+    Console.WriteLine("Employ is Absent");
+}
